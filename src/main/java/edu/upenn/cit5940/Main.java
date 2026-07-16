b4de3a7a99867c59e588231252d4bd01d2aed1c9
Charlee <cgordon5025@gmail.com>
2026-07-15 20:51:47 -0400

    Initial commit

---
+++ src/main/java/edu/upenn/cit5940/Main.java
@@ -0,0 +1,8 @@
+package edu.upenn.cit5940;
+
+public class Main {
+    public static void main(String[] args) {
+        //TAKES IN ARGS FROM LAUNCH OF APP (E.G. MYPROJECT.JAVA FILENAME.CSV)
+
+    }
+}
